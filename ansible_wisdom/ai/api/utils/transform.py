14c140f70c08125c34b15c7cbff4accc577ff2c1
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-01-28 11:27:05 +0530

    add camle to sanke case transform utils

---
+++ ansible_wisdom/ai/api/utils/transform.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict
+
+from requests import request
+
+
+def camel_to_snake(name: str) -> str:
+    snake_case = ""
+    for item in range(len(name)):
+        if name[item].isupper() and item > 0:
+            if name[item-1].islower():
+                snake_case += "_"
+        snake_case += name[item].lower()
+    return snake_case
+
+def convert_keys(original_dict: Dict[str, Any]) -> Dict[str, Any]:
+    converted_dict = {}
+    for key in original_dict.keys():
+        snake_key = camel_to_snake(key)
+        converted_dict[snake_key] = original_dict[key]
+    return converted_dict
