17ead2ab78093cc32f2896436ad62e727691899c
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-02-02 14:45:03 -0500

    Portable database health check
    to ensure that the Django migrations do not run until the database is
    fully up.
    
    The previous solution, to use the long form of `depends_on:` in our
    compose.yaml file, isn't supported by podman-compose:
    https://github.com/containers/podman-compose/pull/453

--- tools/scripts/launch-wisdom.sh
+++ tools/scripts/launch-wisdom.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+until echo -e 'import sys\nfrom django.db import connection\ntry:\n    connection.ensure_connection()\nexcept Exception:\n    sys.exit(1)\n' | /var/www/venv/bin/python ansible_wisdom/manage.py shell
+do
+    echo "Waiting until the database is ready..."
+    sleep 5
+done
+
 /var/www/venv/bin/python ansible_wisdom/manage.py migrate --noinput
 
 cd ansible_wisdom/
