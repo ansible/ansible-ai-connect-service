ae86bf05dd32c18dc13b84c67534c78b8d63cce3
Tami Takamiya <ttakamiy@redhat.com>
2023-02-09 11:23:27 -0500

    Changes based on comments

--- ansible_wisdom/main/urls.py
+++ ansible_wisdom/main/urls.py
@@ -43,10 +43,14 @@ urlpatterns = [
     ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('django_prometheus.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path(
-        'api/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
-    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path(
+            'api/schema/swagger-ui/',
+            SpectacularSwaggerView.as_view(url_name='schema'),
+            name='swagger-ui',
+        ),
+    ]
