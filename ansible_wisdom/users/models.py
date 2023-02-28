7d265290847f68ff49f5063cd44543784b764ef1
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-02-28 15:26:25 -0500

    Remove unused imports from the users app

--- ansible_wisdom/users/models.py
+++ ansible_wisdom/users/models.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
--- ansible_wisdom/users/views.py
+++ ansible_wisdom/users/views.py
@@ -1,12 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-from requests.exceptions import HTTPError
-from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from social_django.utils import psa
 
 
 def home(request):
