77ff5d4749dc4a555dee79d16cf32febf1a78928
hirokuni-kitahara <hirokuni.kitahara1@ibm.com>
2023-03-02 01:06:46 +0900

    fix a bug in views.py
    Signed-off-by: hirokuni-kitahara <hirokuni.kitahara1@ibm.com>

--- ansible_wisdom/ai/api/views.py
+++ ansible_wisdom/ai/api/views.py
@@ -1,5 +1,4 @@
 # Create your views here.
-import json
 import logging
 import time
 
@@ -93,7 +92,6 @@ class Completions(APIView):
 
                 exception = None
                 postprocessed_yaml = None
-                postprocess_detail = None
                 try:
                     # if the recommentation is not a valid yaml, record it as an exception
                     if recommendation_problem:
@@ -104,16 +102,12 @@ class Completions(APIView):
                             f"suggestion id: {suggestion_id}, "
                             f"original recommendation: \n{recommendation_yaml}"
                         )
-                        postprocessed_yaml, postprocess_detail = ari_caller.postprocess(
+                        postprocessed_yaml = ari_caller.postprocess(
                             recommendation_yaml, prompt, context
                         )
                         logger.debug(
                             f"suggestion id: {suggestion_id}, "
                             f"post-processed recommendation: \n{postprocessed_yaml}"
-                        )
-                        logger.debug(
-                            f"suggestion id: {suggestion_id}, "
-                            f"post-process detail: {json.dumps(postprocess_detail)}"
                         )
                         recommendation["predictions"][i] = postprocessed_yaml
                 except Exception as exc:
@@ -129,7 +123,6 @@ class Completions(APIView):
                         suggestion_id,
                         recommendation_yaml,
                         postprocessed_yaml,
-                        postprocess_detail,
                         exception,
                         start_time,
                     )
@@ -145,7 +138,6 @@ class Completions(APIView):
         suggestion_id,
         recommendation_yaml,
         postprocessed_yaml,
-        postprocess_detail,
         exception,
         start_time,
     ):
@@ -158,7 +150,6 @@ class Completions(APIView):
                 "duration": duration,
                 "recommendation": recommendation_yaml,
                 "postprocessed": postprocessed_yaml,
-                "detail": postprocess_detail,
                 "suggestionId": str(suggestion_id) if suggestion_id else None,
             }
 
