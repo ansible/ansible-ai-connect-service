986d2859690718bbb23ea3ef1487063ec5eda1e4
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-03-03 15:38:36 -0500

    Fix an error with the conditional Token import

--- ansible_wisdom/users/views.py
+++ ansible_wisdom/users/views.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.views.generic import TemplateView
 
+# OAUTH: remove when switched
+if not settings.OAUTH2_ENABLE:
+    from rest_framework.authtoken.models import Token
+
 
 class UserTemplateView(TemplateView):
     template_name = 'users/home.html'
@@ -8,7 +12,6 @@ class UserTemplateView(TemplateView):
 
     # OAUTH: remove when switched
     if not settings.OAUTH2_ENABLE:
-        from rest_framework.authtoken.models import Token
 
         def get_context_data(self, **kwargs):
             kwargs = super().get_context_data(**kwargs)
