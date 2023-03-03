ba5e2a642450b760341f31ccc2d8c866b32e07a7
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-03-03 15:22:53 -0500

    Oauth integration (#84)
    * Install and do basic setup of django-oauth-toolkit
    
    - add django-oauth-toolkit to the Python dependencies
    - add oauth2_provider to INSTALLED_APPS
    - add OAuth2Backend to the Django auth backends
    - add OAuth2Authentication to the DRF auth classes
    - add the provider urls to the urlconf
    - add the OAuth2 token middleware
    - configure refresh tokens to expire after 2 weeks
    - configure the available OAuth2 scopes
    
    bonus:
    
    - add SessionAuthentication to the DRF authentication classes, to
      allow for service accounts
    - consolidate the SOCIAL_AUTH_JSONFIELD_ENABLED setting into base.py,
      since we don't change it per-environment and don't populate it from
      env vars
    
    * Switch the home view to using a plain Django TemplateView
    
    * Make sure that the Github login link respects our `next` param
    
    This is needed if a non-logged-in user attempts to hit the
    /o/authorize endpoint.  We want to successfully chain the SSO and
    OAuth2 flows.
    
    * Mark the completions endpoint as requiring read-write token scope
    
    * Hide the new OAuth2 flow behind a setting + env var

--- ansible_wisdom/users/models.py
+++ ansible_wisdom/users/models.py
@@ -1,15 +1,20 @@
-from django.conf import settings
+import os
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
     pass
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# OAUTH: remove block
+if not bool(os.getenv('OAUTH2_ENABLE')):
+    from django.conf import settings
+    from django.db.models.signals import post_save
+    from django.dispatch import receiver
+    from rest_framework.authtoken.models import Token
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
