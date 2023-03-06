ab72aad6220b62219e96e177113e37ceafeb0288
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-03-06 16:45:49 -0500

    Adjust the AI model and admin
    - show the created and updated timestamp fields as read-only in the admin
    - make the description field optional
    - make the inference and management fields validated URLFields

--- ansible_wisdom/ai/admin.py
+++ ansible_wisdom/ai/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin  # noqa
 
 from .models import AIModel
 
-admin.site.register(AIModel)
+
+class AIAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(AIModel, AIAdmin)
--- ansible_wisdom/ai/models.py
+++ ansible_wisdom/ai/models.py
@@ -9,14 +9,14 @@ class AIModel(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
     version = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     model_mesh_api_type = models.CharField(
         max_length=4, choices=ModelMeshType.choices, default=ModelMeshType.HTTP
     )
-    inference_url = models.TextField()
-    management_url = models.TextField()
+    inference_url = models.URLField()
+    management_url = models.URLField()
 
     def __str__(self):
         return self.name
