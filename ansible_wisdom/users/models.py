921d7e8a9db5de36d6f26db2152e56a715293c3e
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-03-09 16:39:25 -0500

    Add a new uuid field to the user model (#139)
    for sending an anonymized identifier to Segment.

--- ansible_wisdom/users/models.py
+++ ansible_wisdom/users/models.py
@@ -1,10 +1,12 @@
 import os
+import uuid
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
 
 # OAUTH: remove block
