861256ea1790af33fa1b91eda6596ce967fcac79
Tami Takamiya <ttakamiy@redhat.com>
2023-03-14 16:33:13 -0400

    Update based on review comments

--- ansible_wisdom/healthcheck/version_info.py
+++ ansible_wisdom/healthcheck/version_info.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from pathlib import Path
 
 
 class VersionInfo:
@@ -6,14 +7,17 @@ class VersionInfo:
     _git_commit = 'git-commit-not-defined'
 
     def __init__(self):
-        with open('version_info.ini') as INI:
-            try:
+        try:
+            ini_file = Path(__file__).parent.parent / 'version_info.ini'
+            with open(ini_file) as INI:
                 configparser = ConfigParser()
-                configparser.read('version_info.ini')
+                configparser.read_file(INI)
                 self._image_tags = configparser['ansible-wisdom-service']['IMAGE_TAGS']
                 self._git_commit = configparser['ansible-wisdom-service']['GIT_COMMIT']
-            finally:
-                return
+        except Exception:
+            pass
+        finally:
+            return
 
     @property
     def image_tags(self):
