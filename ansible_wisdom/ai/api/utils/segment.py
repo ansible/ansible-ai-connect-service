9fddc982a3649b64f114f6e50fb2f93d5e97495a
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-03-14 19:30:05 +0530

    remove unwanted logs

--- ansible_wisdom/ai/api/utils/segment.py
+++ ansible_wisdom/ai/api/utils/segment.py
@@ -15,9 +15,6 @@ def send_segement_event(event: Dict[str, Any], event_name: str, user_id: Union[s
     analytics.write_key = "5r1FsJAkhB1ZWnxxaMRPDav5RdVnyMLr"
     analytics.debug = settings.DEBUG
     analytics.send = True
-    print("sending segment user_id: %s", user_id)
-    print("sending segment event: %s", event)
-    print("sending segment event_name: %s", event_name)
     analytics.track(
         str(user_id) if user_id else 'unknown',
         event_name,
