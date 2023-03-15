ca69b3ee73bbcd7ccc0bf33657ef0988a7fd7f92
Ganesh Nalawade <ganesh634@gmail.com>
2023-03-15 08:45:27 +0530

    Add feedback API endpoint (#162)
    * Add feedback API endpoint
    
    *  Feedback data for inline suggestion
       - latency at client side
       - Time taken by user to take any action (accept/ignore)
         after inline suggestion was displayed
       - User action (accept/reject)
       - Unique suggestion ID
    *  Feedback data for Ansible content
       - The entire file content
       - Document URI
       - The client trigger that caused file upload. Trigger
         can be one of file open, file close and tab change.
    *  Add utils function to send data to segment
    
    * fix flake8 issues
    
    * update openapi schema file
    
    * fix lint issue
    
    * remove unwanted logs
    
    * add unit test and fix review comment
    
    * get user_id from request
    
    * covert uuid to string

---
+++ ansible_wisdom/ai/api/utils/segment.py
@@ -0,0 +1,23 @@
+import logging
+from typing import Any, Dict, Union
+
+from django.conf import settings
+from segment import analytics
+
+logger = logging.getLogger(__name__)
+
+
+def send_segment_event(event: Dict[str, Any], event_name: str, user_id: Union[str, None]) -> None:
+    if not settings.SEGMENT_WRITE_KEY:
+        logger.info("segment write key not set, skipping event")
+        return
+
+    analytics.write_key = settings.SEGMENT_WRITE_KEY
+    analytics.debug = settings.DEBUG
+    analytics.send = True
+    analytics.track(
+        str(user_id) if user_id else 'unknown',
+        event_name,
+        event,
+    )
+    logger.info("sent segment event: %s", event_name)
