b026bab9440d4e58127bc8f4c044e49aa5199a64
Gonéri Le Bouder <goneri@lebouder.net>
2023-03-27 17:23:35 -0400

    add a command to create the token

---
+++ ansible_wisdom/users/management/commands/createtoken.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.timezone import now
+from oauth2_provider.models import AccessToken, Application, RefreshToken
+from oauthlib.common import generate_token
+
+
+class Command(BaseCommand):
+    help = "Create an accesstoken for API access (e.g: to run the tests)"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str, help="User associated with the token")
+        parser.add_argument('token_name', type=str, help="Name of the token")
+        parser.add_argument(
+            '--create-user', action='store_true', help="Also create the user.", default=False
+        )
+        parser.add_argument(
+            '--duration', type=int, help="How long the token is valid (minute)", default=60
+        )
+
+    def handle(self, username, token_name, duration, create_user, *args, **options):
+        u = None
+        User = get_user_model()
+        if not User.objects.filter(username=username).exists():
+            if create_user:
+                u = User.objects.create_user(username=username, date_terms_accepted=now())
+            else:
+                self.stderr.write(f"Cannot find user {username}!")
+                return
+        else:
+            u = User.objects.get(username=username)
+
+        if not u.date_terms_accepted:
+            self.stdout.write(f"Token {token_name} already exists")
+            return
+
+        if AccessToken.objects.filter(token=token_name).exists():
+            self.stdout.write(f"Token {token_name} already exists")
+            return
+
+        self.stdout.write(f"Creating a new token called {token_name}")
+        token = AccessToken.objects.create(
+            token=token_name,
+            user=u,
+            scope="read write",
+            expires=now() + datetime.timedelta(minutes=duration),
+        )
