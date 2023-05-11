d3cf10d81dbdf5cd9b139ae4d37179bf60c93e70
Sumit Jaiswal <sjaiswal@redhat.com>
2023-05-12 01:08:50 +0530

    fix port range
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- ansible_wisdom/main/settings/base.py
+++ ansible_wisdom/main/settings/base.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    # "django_prometheus.middleware.PrometheusBeforeMiddleware",
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "social_django.middleware.SocialAuthExceptionMiddleware",
     "main.middleware.SegmentMiddleware",
-    # "django_prometheus.middleware.PrometheusAfterMiddleware"
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -235,7 +235,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-PROMETHEUS_METRICS_EXPORT_PORT_RANGE = range(8001, 8021)
+PROMETHEUS_METRICS_EXPORT_PORT_RANGE = range(8001, 8011)
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
