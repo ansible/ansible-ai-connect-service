d6dcf140be405d93a21a3c564ead349f074ba38e
Tami Takamiya <ttakamiy@redhat.com>
2023-05-31 04:44:02 +0900

    Add Prometheus monitoring to User model

--- ansible_wisdom/users/models.py
+++ ansible_wisdom/users/models.py
@@ -3,8 +3,9 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
 
 
-class User(AbstractUser):
+class User(ExportModelOperationsMixin('user'), AbstractUser):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     date_terms_accepted = models.DateTimeField(unique=False, default=None, null=True)
