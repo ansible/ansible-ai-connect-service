ff7c740f1e187036be6833c8380fb4e301ee3505
Tami Takamiya <ttakamiy@redhat.com>
2023-06-02 08:18:54 +0900

    Add more logs to Terms of Service page 500 error

--- ansible_wisdom/users/views.py
+++ ansible_wisdom/users/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from django.conf import settings
 from django.forms import Form
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from django.urls import reverse
 from django.views.generic import TemplateView
 from rest_framework.generics import RetrieveAPIView
@@ -44,17 +45,26 @@ class TermsOfService(TemplateView):
     def get(self, request, *args, **kwargs):
         partial_token = request.GET.get('partial_token')
         self.extra_context['partial_token'] = partial_token
+        if partial_token is None:
+            logger.error('GET /terms_of_service/ was invoked without partial_token')
+            return HttpResponseForbidden()
         return super().get(request, args, kwargs)
 
     def post(self, request, *args, **kwargs):
         form = Form(request.POST)
         form.is_valid()
         partial_token = form.data.get('partial_token')
+        if partial_token is None:
+            logger.error('POST /terms_of_service/ was invoked without partial_token')
+            return HttpResponseBadRequest()
 
         accepted = request.POST.get('accepted') == 'True'
 
         strategy = load_strategy()
         partial = strategy.partial_load(partial_token)
+        if partial is None:
+            logger.error('strategy.partial_load(partial_token) returned None')
+            return HttpResponseBadRequest()
         backend = partial.backend
 
         if accepted:
