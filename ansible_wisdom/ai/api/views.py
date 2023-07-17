bdffaaa4184404476a14c1d61e285da868635ccf
Tami Takamiya <ttakamiy@redhat.com>
2023-07-17 15:27:00 -0400

    Give a larger rate limit to the feedback endpoint

--- ansible_wisdom/ai/api/views.py
+++ ansible_wisdom/ai/api/views.py
@@ -419,6 +419,7 @@ class Feedback(APIView):
     required_scopes = ['read', 'write']
 
     throttle_cache_key_suffix = '_feedback'
+    throttle_cache_multiplier = 6.0
 
     @extend_schema(
         request=FeedbackRequestSerializer,
--- ansible_wisdom/users/throttling.py
+++ ansible_wisdom/users/throttling.py
@@ -16,6 +16,9 @@ class GroupSpecificThrottle(UserRateThrottle):
     # The attribute name that may be defined in views to add a suffix to cache keys
     cache_key_suffix_attr = 'throttle_cache_key_suffix'
 
+    # The attribute name that may be definied in views to give a larger (or smaller) rate limit
+    cache_key_multipiler_attr = 'throttle_cache_multiplier'
+
     def __init__(self):
         # Override, since we can't decide what the scope and rate are
         # until we see the request.
@@ -32,7 +35,7 @@ class GroupSpecificThrottle(UserRateThrottle):
 
         # get_rate() will now pull the rate setting based on our dynamically set
         # scope value.
-        self.rate = self.get_rate()
+        self.rate = self.get_rate(view)
         self.num_requests, self.duration = self.parse_rate(self.rate)
 
         return super().allow_request(request, view)
@@ -44,3 +47,25 @@ class GroupSpecificThrottle(UserRateThrottle):
         if cache_key_suffix:
             cache_key += cache_key_suffix
         return cache_key
+
+    def get_rate(self, view):
+        rate = super().get_rate()
+
+        multipiler = getattr(view, self.cache_key_multipiler_attr, None)
+        # If a multiplier is defined in the view, modify the rate based on it
+        if multipiler:
+            num_requests, duration = self.parse_rate(rate)
+            rate = GroupSpecificThrottle.format_rate(
+                int(float(multipiler) * num_requests), duration
+            )
+        return rate
+
+    @staticmethod
+    def format_rate(num_requests, duration):
+        duration_unit = {
+            1: 'second',
+            60: 'minute',
+            3600: 'hour',
+            86400: 'day',
+        }[duration]
+        return f'{num_requests}/{duration_unit}'
