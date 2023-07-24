0c0f1ed82aea234bdad817cfc2a7cab9ee8fe2c3
Siddharth Rajaraman <siddharthrajaraman@Siddharths-MacBook-Pro.local>
2023-07-24 10:49:20 -0400

    switching to dynatrace

--- ansible_wisdom/ai/api/utils/jaeger.py
+++ ansible_wisdom/ai/api/utils/jaeger.py
@@ -1,23 +1,76 @@
+# import inspect
+#
+# from django.conf import settings
+# from opentelemetry import trace
+# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+#
+# trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "REQUEST"})))
+#
+# jaeger_exporter = JaegerExporter(
+#     agent_host_name='localhost',
+#     agent_port=6831,
+# )
+#
+# span_processor = BatchSpanProcessor(jaeger_exporter)
+# trace.get_tracer_provider().add_span_processor(span_processor)
+# tracer = trace.get_tracer(__name__)
+import json
 import inspect
-
+from opentelemetry.sdk.resources import Resource
 from django.conf import settings
+
+# Import exporters
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+# Trace imports
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.trace import set_tracer_provider, get_tracer_provider
+from opentelemetry.sdk.trace import TracerProvider, sampling
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "REQUEST"})))
+# ===== GENERAL SETUP =====
 
-jaeger_exporter = JaegerExporter(
-    agent_host_name='localhost',
-    agent_port=6831,
+DT_API_URL = "https://win89656.live.dynatrace.com/api/v2/otlp"
+DT_API_TOKEN = "dt0c01.BDJQZ4B5GF4TVLQNOL22UHLK.GVEBHIVB427YF5MSH2KTSFNNEKCICSLU7IB2GHNINUUQXBMJ3CA4FC7HLJRYGW4X"
+
+
+merged = dict()
+for name in ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.json", "/var/lib/dynatrace/enrichment/dt_metadata.json"]:
+  try:
+    data = ''
+    with open(name) as f:
+      data = json.load(f if name.startswith("/var") else open(f.read()))
+      merged.update(data)
+  except:
+    pass
+
+merged.update({
+  "service.name": "Ansible Lightspeed", #TODO Replace with the name of your application
+  "service.version": "1.0.1", #TODO Replace with the version of your application
+})
+resource = Resource.create(merged)
+
+
+# ===== TRACING SETUP =====
+
+tracer_provider = TracerProvider(sampler=sampling.ALWAYS_ON, resource=resource)
+trace.set_tracer_provider(tracer_provider)
+
+tracer_provider.add_span_processor(
+  BatchSpanProcessor(
+    OTLPSpanExporter(
+      endpoint = DT_API_URL + "/v1/traces",
+      headers = {
+        "Authorization": "Api-Token " + DT_API_TOKEN
+      }
+    )
+  )
 )
 
-span_processor = BatchSpanProcessor(jaeger_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
-tracer = trace.get_tracer(__name__)
-
+tracer = get_tracer_provider().get_tracer("my-tracer")
 
 def distributed_tracing_method(name, description, file, method, span_ctx):
     inner_span_ctx = None
