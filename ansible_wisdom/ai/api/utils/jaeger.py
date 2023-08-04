536153191441fa0ebc52ceb8cfbf61388cfbc937
Siddharth Rajaraman <sidrajaraman@gmail.com>
2023-08-04 08:59:29 -0400

    distributed tracing with otel and dynatrace

--- ansible_wisdom/ai/api/utils/jaeger.py
+++ ansible_wisdom/ai/api/utils/jaeger.py
@@ -1,22 +1,3 @@
-# import inspect
-#
-# from django.conf import settings
-# from opentelemetry import trace
-# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor
-#
-# trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "REQUEST"})))
-#
-# jaeger_exporter = JaegerExporter(
-#     agent_host_name='localhost',
-#     agent_port=6831,
-# )
-#
-# span_processor = BatchSpanProcessor(jaeger_exporter)
-# trace.get_tracer_provider().add_span_processor(span_processor)
-# tracer = trace.get_tracer(__name__)
 import json
 import inspect
 from opentelemetry.sdk.resources import Resource
@@ -72,6 +53,9 @@ tracer_provider.add_span_processor(
 
 tracer = get_tracer_provider().get_tracer("my-tracer")
 
+
+
+#tracing method
 def distributed_tracing_method(name, description, file, method, span_ctx):
     inner_span_ctx = None
     if settings.ENABLE_DISTRIBUTED_TRACING:
@@ -84,6 +68,10 @@ def distributed_tracing_method(name, description, file, method, span_ctx):
     return inner_span_ctx
 
 
+
+
+
+#wrapper method
 def with_distributed_tracing(name, description, file, method):
     def distributed_tracing_decorator(func):
         def distributed_tracing_wrapper(self, *args, **kwargs):
