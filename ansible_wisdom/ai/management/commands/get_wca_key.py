9a62251470115e2a3e3a56431dafab3a057fbc2e
Peter Braun <pbraun@redhat.com>
2023-08-17 12:52:02 +0200

    add custom commands to get, post and delete wca api keys

---
+++ ansible_wisdom/ai/management/commands/get_wca_key.py
@@ -0,0 +1,37 @@
+from ai.api.aws.wca_secret_manager import WcaSecretManager, WcaSecretManagerError
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Create WCA API Key for OrgId"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "org_id", type=str, help="The Red Hat OrgId that the API Key belongs to"
+        )
+
+    def handle(self, *args, **options):
+        org_id = options["org_id"]
+
+        client = WcaSecretManager(
+            settings.WCA_SECRET_MANAGER_ACCESS_KEY,
+            settings.WCA_SECRET_MANAGER_SECRET_ACCESS_KEY,
+            settings.WCA_SECRET_MANAGER_KMS_KEY_ID,
+            settings.WCA_SECRET_MANAGER_PRIMARY_REGION,
+            settings.WCA_SECRET_MANAGER_REPLICA_REGIONS,
+        )
+
+        self.stdout.write(f"Using AWS Primary Region: {settings.WCA_SECRET_MANAGER_PRIMARY_REGION}")
+
+        try:
+            response = client.get_key(org_id)
+            if response is None:
+                self.stdout.write(f"No API Key for orgId '{org_id}' found.")
+                return
+
+            self.stdout.write(
+                f"API Key for orgId '{org_id}' found. Last updated: {response['CreatedDate']}"
+            )
+        except WcaSecretManagerError as e:
+            raise CommandError(e)
---
+++ ansible_wisdom/ai/management/commands/post_wca_key.py
@@ -0,0 +1,36 @@
+from ai.api.aws.wca_secret_manager import WcaSecretManager, WcaSecretManagerError
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Create WCA API Key for OrgId"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "org_id", type=str, help="The Red Hat OrgId that the API Key belongs to"
+        )
+        parser.add_argument('key', type=str, help="IBM WCA API Key")
+
+    def handle(self, *args, **options):
+        org_id = options["org_id"]
+        key = options["key"]
+
+        client = WcaSecretManager(
+            settings.WCA_SECRET_MANAGER_ACCESS_KEY,
+            settings.WCA_SECRET_MANAGER_SECRET_ACCESS_KEY,
+            settings.WCA_SECRET_MANAGER_KMS_KEY_ID,
+            settings.WCA_SECRET_MANAGER_PRIMARY_REGION,
+            settings.WCA_SECRET_MANAGER_REPLICA_REGIONS,
+        )
+
+        self.stdout.write(f"Using AWS Primary Region: {settings.WCA_SECRET_MANAGER_PRIMARY_REGION}")
+        self.stdout.write(
+            f"Using AWS Replica Region(s): {settings.WCA_SECRET_MANAGER_REPLICA_REGIONS}"
+        )
+
+        try:
+            key_name = client.save_key(org_id, key)
+            self.stdout.write(f"API Key for orgId '{org_id}' stored as: {key_name}")
+        except WcaSecretManagerError as e:
+            raise CommandError(e)
