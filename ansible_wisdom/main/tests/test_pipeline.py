8db44bbd1f0c5d0fa5311ea7bfb83bc5238b360f
Richard Gebhardt <gebhardt@redhat.com>
2023-08-17 12:04:20 -0400

    add pipeline tests (#451)
    Signed-off-by: Richard Gebhardt <gebhardtr@redhat.com>

---
+++ ansible_wisdom/main/tests/test_pipeline.py
@@ -0,0 +1,22 @@
+from unittest import TestCase, mock
+
+import main.pipeline as pipeline
+
+
+class TestPipeline(TestCase):
+    def test_remove_pii(self):
+        details = {
+            "email": "",
+            "fullname": "",
+            "first_name": "",
+            "last_name": "",
+            "favorite_color": "chartreuse",
+        }
+
+        pipeline.remove_pii(None, details, None, None, None)
+        self.assertEqual(
+            details,
+            {
+                "favorite_color": "chartreuse",
+            },
+        )
