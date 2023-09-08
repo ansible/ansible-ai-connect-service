eea87e37be0d97dcfe6b9c6b74d18c5c6ada725a
Richard Gebhardt <gebhardtr@redhat.com>
2023-09-08 11:29:56 -0400

    use correct attribute
    Signed-off-by: Richard Gebhardt <gebhardtr@redhat.com>

--- ansible_wisdom/ai/api/serializers.py
+++ ansible_wisdom/ai/api/serializers.py
@@ -362,8 +362,8 @@ class EnumField(serializers.Field):
 class AttributionSerializer(serializers.Serializer):
     repo_name = serializers.CharField()
     repo_url = serializers.URLField()
-    path = serializers.CharField(required=False)
-    license = serializers.CharField(required=False)
+    path = serializers.CharField(allow_blank=True)
+    license = serializers.CharField()
     data_source = EnumField(choices=DataSource)
     ansible_type = EnumField(choices=AnsibleType)
     score = serializers.FloatField()
