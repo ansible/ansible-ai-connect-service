f42ce1208d09629b4537122a47128e4e74df1cab
Gonéri Le Bouder <goneri@lebouder.net>
2023-09-11 17:25:35 -0400

    SSO: redirect auth user to the main page (#513)
    We don't use `redirect_authenticated_user` to avoid giving other websites the ability
    to build a "social media fingerprint".
    
    ```
    If you enable redirect_authenticated_user, other websites will be able to determine
    if their visitors are authenticated on your site by requesting redirect URLs to
    image files on your website.
    ```
    
    https://docs.djangoproject.com/en/4.2/topics/auth/default/#django.contrib.auth.views.LoginView.redirect_authenticated_user

---
+++ ansible_wisdom/main/views.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import logging
+from datetime import datetime
+
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponseRedirect
+
+logger = logging.getLogger(__name__)
+
+
+class LoginView(auth_views.LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect("/")
+        return super().dispatch(request, *args, **kwargs)
