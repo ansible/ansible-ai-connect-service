2fa27190d34c25a970719b92324aa05d09d1b024
Michael Anstis <manstis@redhat.com>
2023-09-12 09:49:17 +0100

    Fix tests.

--- ansible_wisdom/main/tests/test_console_views.py
+++ ansible_wisdom/main/tests/test_console_views.py
@@ -4,11 +4,17 @@ from unittest.mock import patch
 from ai.api.permissions import (
     IsOrganisationAdministrator,
     IsOrganisationLightspeedSubscriber,
+    IsWCAKeyApiFeatureFlagOn,
+    IsWCAModelIdApiFeatureFlagOn,
 )
 from ai.api.tests.test_views import WisdomServiceAPITestCaseBase
+from django.test import override_settings
 from django.urls import reverse
 
 
+@override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+@patch.object(IsWCAKeyApiFeatureFlagOn, 'has_permission', return_value=True)
+@patch.object(IsWCAModelIdApiFeatureFlagOn, 'has_permission', return_value=True)
 @patch.object(IsOrganisationAdministrator, 'has_permission', return_value=True)
 @patch.object(IsOrganisationLightspeedSubscriber, 'has_permission', return_value=True)
 class TestConsoleView(WisdomServiceAPITestCaseBase):
