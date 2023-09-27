e89ca7721efb55477a06db30b7caadf464e0f9fb
Michael Anstis <manstis@users.noreply.github.com>
2023-09-27 12:48:29 +0100

    AAP-16386: Adjust model server timeout for multitask completion requests (#574)
    Co-authored-by: Michael Anstis <manstis@redhat.com>

--- ansible_wisdom/ai/api/model_client/http_client.py
+++ ansible_wisdom/ai/api/model_client/http_client.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 import requests
+from ai.api.formatter import get_task_names_from_prompt
 from django.conf import settings
 
 from .base import ModelMeshClient
@@ -20,13 +21,19 @@ class HttpClient(ModelMeshClient):
         model_id = model_id or settings.ANSIBLE_AI_MODEL_NAME
         self._prediction_url = f"{self._inference_url}/predictions/{model_id}"
 
+        prompt = model_input.get("instances", [{}])[0].get("prompt", "")
+
         try:
+            task_count = len(get_task_names_from_prompt(prompt))
             result = self.session.post(
-                self._prediction_url, headers=self.headers, json=model_input, timeout=self.timeout
+                self._prediction_url,
+                headers=self.headers,
+                json=model_input,
+                timeout=self.timeout(task_count),
             )
             result.raise_for_status()
             response = json.loads(result.text)
             response['model_id'] = model_id
             return response
-        except requests.exceptions.ReadTimeout:
+        except requests.exceptions.Timeout:
             raise ModelTimeoutError
