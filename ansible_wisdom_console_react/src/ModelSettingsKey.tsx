00a0aa8fab77bb768054698ed386094db78569a7
Michael Anstis <manstis@users.noreply.github.com>
2023-10-05 13:56:33 +0100

    AAP-16576: Console UI: Increase Unit Test coverage: Remove duplications. (#607)
    Co-authored-by: Michael Anstis <manstis@redhat.com>

--- ansible_wisdom_console_react/src/ModelSettingsKey.tsx
+++ ansible_wisdom_console_react/src/ModelSettingsKey.tsx
@@ -1,32 +1,10 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {
-    Alert,
-    Breadcrumb,
-    BreadcrumbItem,
-    Button,
-    Icon,
-    PageSection,
-    PageSectionVariants,
-    Panel,
-    PanelMain,
-    PanelMainBody,
-    Split,
-    SplitItem,
-    Stack,
-    StackItem,
-    Text,
-    TextContent,
-    TextInput,
-    Tooltip
-} from "@patternfly/react-core";
-import {useTranslation} from "react-i18next";
-import {CheckCircleIcon, OutlinedQuestionCircleIcon} from "@patternfly/react-icons";
 import './ModelSettings.css';
 import {WcaKey, WcaKeyRequest} from "./api/types";
 import {saveWcaKey} from "./api/api";
-import {ErrorModal, HasError, NO_ERROR} from "./ErrorModal";
+import {HasError, NO_ERROR} from "./ErrorModal";
 import {DELAY} from "./api/globals";
-import {BusyButton} from "./BusyButton";
+import {ModelSettingsEditor} from "./ModelSettingsEditor";
 
 interface ModelSettingsKeyProps {
     wcaKey: WcaKey | undefined;
@@ -35,13 +13,10 @@ interface ModelSettingsKeyProps {
 }
 
 export const ModelSettingsKey = (props: ModelSettingsKeyProps) => {
-    const {t} = useTranslation();
     const {wcaKey, cancel, reload} = props;
 
-    const [value, setValue] = useState<string>("");
     const [isSaving, setSaving] = useState<boolean>(false);
-    const isSaveDisabled: boolean = useMemo(() => value?.trim().length === 0 || isSaving, [value, isSaving]);
-    const hasWcaKey = useMemo(() => wcaKey !== undefined, [wcaKey]);
+    const hasWcaKey = useMemo(() => wcaKey?.status === "SUCCESS", [wcaKey]);
 
     const [isKeyInvalid, setIsKeyInvalid] = useState<boolean>(false);
     const [keyError, setKeyError] = useState<HasError>(NO_ERROR);
@@ -69,107 +44,27 @@ export const ModelSettingsKey = (props: ModelSettingsKeyProps) => {
     }, [reload]);
 
     return (
-        <>
-            <ErrorModal
-                message={t("KeyError")}
-                hasError={keyError}
-                close={() => setKeyError(NO_ERROR)}
-            />
-            <PageSection
-                variant={PageSectionVariants.light}
-                isWidthLimited
-                data-testid={"model-settings-key__bread-crumbs"}
-            >
-                <Stack hasGutter={true}>
-                    <StackItem>
-                        <Breadcrumb>
-                            <BreadcrumbItem to="/console/admin/settings">{t("ModelSettings")}</BreadcrumbItem>
-                            <BreadcrumbItem isActive>{t("AddKeyTitle")}</BreadcrumbItem>
-                        </Breadcrumb>
-                    </StackItem>
-                    <StackItem>
-                        <TextContent>
-                            {!hasWcaKey && (
-                                <Text component="h1">{t("AddKeyTitle")}</Text>
-                            )}
-                            {hasWcaKey && (
-                                <Text component="h1">{t("UpdateKeyTitle")}</Text>
-                            )}
-                        </TextContent>
-                    </StackItem>
-                </Stack>
-            </PageSection>
-            <PageSection data-testid={"model-settings-key__editor"}>
-                <Panel variant={"bordered"}>
-                    <PanelMain>
-                        <PanelMainBody>
-                            <Stack hasGutter={true}>
-                                {isKeyInvalid && (
-                                    <StackItem>
-                                        <Alert
-                                            variant="warning"
-                                            title={t("KeyInvalidAlert")}
-                                            data-testid={"model-settings-key__alert-key-invalid"}
-                                        />
-                                    </StackItem>
-                                )}
-                                <StackItem>
-                                    <TextContent>
-                                        <Text component={"h3"}>
-                                            {t("APIKey")}
-                                            <Tooltip aria="none" aria-live="polite" content={t("APIKeyTooltip")}>
-                                                <Icon>
-                                                    <OutlinedQuestionCircleIcon className={"Info-icon"}/>
-                                                </Icon>
-                                            </Tooltip>
-                                        </Text>
-                                    </TextContent>
-                                </StackItem>
-                                <StackItem>
-                                    <TextInput
-                                        value={value}
-                                        type="text"
-                                        onChange={(value) => {
-                                            setValue(value);
-                                            setIsKeyInvalid(false);
-                                        }}
-                                        aria-label={t("AddKey")}
-                                        placeholder={t("PlaceholderKey")}
-                                        isDisabled={isSaving}
-                                        data-testid={"model-settings-key__key_textbox"}
-                                    />
-                                </StackItem>
-                                <StackItem>
-                                    <Split hasGutter={true}>
-                                        <SplitItem>
-                                            <BusyButton
-                                                variant={"primary"}
-                                                icon={<CheckCircleIcon/>}
-                                                onClick={() => save(value)}
-                                                isBusy={isSaving}
-                                                isDisabled={isSaveDisabled}
-                                                data-testid={"model-settings-key__save-button"}
-                                            >
-                                                {t("Save")}
-                                            </BusyButton>
-                                        </SplitItem>
-                                        <SplitItem>
-                                            <Button
-                                                variant={"secondary"}
-                                                onClick={cancel}
-                                                isDisabled={isSaving}
-                                                data-testid={"model-settings-key__cancel-button"}
-                                            >
-                                                {t("Cancel")}
-                                            </Button>
-                                        </SplitItem>
-                                    </Split>
-                                </StackItem>
-                            </Stack>
-                        </PanelMainBody>
-                    </PanelMain>
-                </Panel>
-            </PageSection>
-        </>
+        <ModelSettingsEditor
+            hasValue={hasWcaKey}
+            isSaving={isSaving}
+            isValueInvalid={isKeyInvalid}
+            clearInvalidState={() => setIsKeyInvalid(false)}
+            errorState={keyError}
+            setErrorState={setKeyError}
+            save={save}
+            cancel={cancel}
+            captions={
+                {
+                    errorModalCaption: "KeyError",
+                    invalidAlertCaption: "KeyInvalidAlert",
+                    setValueTitle: "AddKeyTitle",
+                    updateValueTitle: "UpdateKeyTitle",
+                    fieldCaption: "APIKey",
+                    fieldCaptionTooltip: "APIKeyTooltip",
+                    fieldInputCaption: "AddKey",
+                    fieldInputPlaceholder: "PlaceholderKey"
+                }
+            }
+        />
     )
 }
--- ansible_wisdom_console_react/src/ModelSettingsModelId.tsx
+++ ansible_wisdom_console_react/src/ModelSettingsModelId.tsx
@@ -1,32 +1,10 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {
-    Alert,
-    Breadcrumb,
-    BreadcrumbItem,
-    Button,
-    Icon,
-    PageSection,
-    PageSectionVariants,
-    Panel,
-    PanelMain,
-    PanelMainBody,
-    Split,
-    SplitItem,
-    Stack,
-    StackItem,
-    Text,
-    TextContent,
-    TextInput,
-    Tooltip
-} from "@patternfly/react-core";
-import {useTranslation} from "react-i18next";
-import {CheckCircleIcon, OutlinedQuestionCircleIcon} from "@patternfly/react-icons";
 import './ModelSettings.css';
 import {WcaModelId, WcaModelIdRequest} from "./api/types";
 import {saveWcaModelId} from "./api/api";
-import {ErrorModal, HasError, NO_ERROR} from "./ErrorModal";
+import {HasError, NO_ERROR} from "./ErrorModal";
 import {DELAY} from "./api/globals";
-import {BusyButton} from "./BusyButton";
+import {ModelSettingsEditor} from "./ModelSettingsEditor";
 
 interface ModelSettingsModelIdProps {
     wcaModelId: WcaModelId;
@@ -35,13 +13,10 @@ interface ModelSettingsModelIdProps {
 }
 
 export const ModelSettingsModelId = (props: ModelSettingsModelIdProps) => {
-    const {t} = useTranslation();
     const {wcaModelId, cancel, reload} = props;
 
-    const [value, setValue] = useState<string>("");
     const [isSaving, setSaving] = useState(false);
-    const isSaveDisabled: boolean = useMemo(() => value?.trim().length === 0 || isSaving, [value, isSaving]);
-    const hasWcaModelId = useMemo(() => wcaModelId !== undefined, [wcaModelId]);
+    const hasWcaModelId = useMemo(() => wcaModelId?.status === "SUCCESS", [wcaModelId]);
 
     const [isModelIdInvalid, setIsModelIdInvalid] = useState<boolean>(false);
     const [modelIdError, setModelIdError] = useState<HasError>(NO_ERROR);
@@ -69,105 +44,27 @@ export const ModelSettingsModelId = (props: ModelSettingsModelIdProps) => {
     }, [reload]);
 
     return (
-        <>
-            <ErrorModal
-                message={t("ModelIdError")}
-                hasError={modelIdError}
-                close={() => setModelIdError(NO_ERROR)}
-            />
-            <PageSection
-                variant={PageSectionVariants.light}
-                isWidthLimited
-                data-testid={"model-settings-model-id__bread-crumbs"}
-            >
-                <Stack hasGutter={true}>
-                    <StackItem>
-                        <Breadcrumb>
-                            <BreadcrumbItem to="/console/admin/settings">{t("ModelSettings")}</BreadcrumbItem>
-                            <BreadcrumbItem isActive>{t("AddModelIdTitle")}</BreadcrumbItem>
-                        </Breadcrumb>
-                    </StackItem>
-                    <StackItem>
-                        <TextContent>
-                            {!hasWcaModelId && (
-                                <Text component="h1">{t("AddModelIdTitle")}</Text>
-                            )}
-                            {hasWcaModelId && (
-                                <Text component="h1">{t("UpdateModelIdTitle")}</Text>
-                            )}
-                            <Text component="h1">{t("")}</Text>
-                        </TextContent>
-                    </StackItem>
-                </Stack>
-            </PageSection>
-            <PageSection data-testid={"model-settings-model-id__editor"}>
-                <Panel variant={"bordered"}>
-                    <PanelMain>
-                        <PanelMainBody>
-                            <Stack hasGutter={true}>
-                                {isModelIdInvalid && (
-                                    <StackItem>
-                                        <Alert
-                                            variant="warning"
-                                            title={t("ModelIdInvalidAlert")}
-                                            data-testid={"model-settings-model-id__alert-model-id-invalid"}
-                                        />
-                                    </StackItem>
-                                )}
-                                <StackItem>
-                                    <TextContent>
-                                        <Text component={"h3"}>
-                                            {t("ModelId")}
-                                            <Tooltip aria="none" aria-live="polite" content={t("ModelIdTooltip")}>
-                                                <Icon>
-                                                    <OutlinedQuestionCircleIcon className={"Info-icon"}/>
-                                                </Icon>
-                                            </Tooltip>
-                                        </Text>
-                                    </TextContent>
-                                </StackItem>
-                                <StackItem>
-                                    <TextInput
-                                        value={value}
-                                        type="text"
-                                        onChange={(value) => setValue(value)}
-                                        aria-label={t("AddKey")}
-                                        placeholder={t("PlaceholderModelId")}
-                                        isDisabled={isSaving}
-                                        data-testid={"model-settings-model-id__model-id_textbox"}
-                                    />
-                                </StackItem>
-                                <StackItem>
-                                    <Split hasGutter={true}>
-                                        <SplitItem>
-                                            <BusyButton
-                                                variant={"primary"}
-                                                icon={<CheckCircleIcon/>}
-                                                onClick={() => save(value)}
-                                                isBusy={isSaving}
-                                                isDisabled={isSaveDisabled}
-                                                data-testid={"model-settings-model-id__save-button"}
-                                            >
-                                                {t("Save")}
-                                            </BusyButton>
-                                        </SplitItem>
-                                        <SplitItem>
-                                            <Button
-                                                variant={"secondary"}
-                                                onClick={cancel}
-                                                isDisabled={isSaving}
-                                                data-testid={"model-settings-model-id__cancel-button"}
-                                            >
-                                                {t("Cancel")}
-                                            </Button>
-                                        </SplitItem>
-                                    </Split>
-                                </StackItem>
-                            </Stack>
-                        </PanelMainBody>
-                    </PanelMain>
-                </Panel>
-            </PageSection>
-        </>
-    )
+        <ModelSettingsEditor
+            hasValue={hasWcaModelId}
+            isSaving={isSaving}
+            isValueInvalid={isModelIdInvalid}
+            clearInvalidState={() => setIsModelIdInvalid(false)}
+            errorState={modelIdError}
+            setErrorState={setModelIdError}
+            save={save}
+            cancel={cancel}
+            captions={
+                {
+                    errorModalCaption: "ModelIdError",
+                    invalidAlertCaption: "ModelIdInvalidAlert",
+                    setValueTitle: "AddModelIdTitle",
+                    updateValueTitle: "UpdateModelIdTitle",
+                    fieldCaption: "ModelId",
+                    fieldCaptionTooltip: "ModelIdTooltip",
+                    fieldInputCaption: "AddModelId",
+                    fieldInputPlaceholder: "PlaceholderModelId"
+                }
+            }
+        />
+    );
 }
