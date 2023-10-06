0c72c4869df81c95d6c0f12e07b5475030335b93
Michael Anstis <manstis@redhat.com>
2023-10-06 10:33:11 +0100

    Include re-direct when authentication fails.

--- ansible_wisdom/main/tests/test_console_views.py
+++ ansible_wisdom/main/tests/test_console_views.py
@@ -16,7 +16,8 @@ class TestConsoleView(WisdomServiceAPITestCaseBase):
     def test_authentication_error(self, *args):
         # self.client.force_authenticate(user=self.user)
         r = self.client.get(reverse('console'))
-        self.assertEqual(r.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(r.status_code, HTTPStatus.FOUND)
+        self.assertEqual(r.url, '/login')
 
     @patch.object(IsOrganisationAdministrator, 'has_permission', return_value=True)
     @patch.object(IsOrganisationLightspeedSubscriber, 'has_permission', return_value=True)
