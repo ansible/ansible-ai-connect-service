113bb41b529528610e202d9518b8f5dc013189a3
undefined <undefined>
2023-10-26 12:33:31 -0400

    Revert API version to 0.0.7 as it was not a part of GA

--- ansible_wisdom/main/settings/development.py
+++ ansible_wisdom/main/settings/development.py
@@ -38,7 +38,7 @@ if DEBUG:
     SPECTACULAR_SETTINGS = {
         'TITLE': 'Ansible Lightspeed with IBM watsonx Code Assistant.',
         'DESCRIPTION': 'Equip the automation developer at Lightspeed.',
-        'VERSION': '1.0.0',
+        'VERSION': '0.0.7',
         'SERVE_INCLUDE_SCHEMA': False,
         # OTHER SETTINGS
         'TAGS': [
