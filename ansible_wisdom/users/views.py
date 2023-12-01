0e3f721c02ae2504fdf7f98be67870dea40294e1
Gonéri Le Bouder <goneri@lebouder.net>
2023-12-01 07:25:58 -0500

    non-seated user of WCA-ready org should not default to TP model
    Do not default to TP model when end-user is in an org with model settings configured,
    but no seat assignment.
    
    See: https://issues.redhat.com/browse/AAP-18386

--- ansible_wisdom/users/views.py
+++ ansible_wisdom/users/views.py
@@ -34,13 +34,9 @@ class HomeView(TemplateView):
         except WcaSecretManagerMissingCredentialsError:
             return context
 
-        if (
-            self.request.user.is_authenticated
-            and self.request.user.rh_org_has_subscription
-            and self.request.user.organization
-        ):
-            org_has_api_key = bool(
-                secret_manager.get_secret(self.request.user.organization.id, Suffixes.API_KEY)
+        if self.request.user.is_authenticated and self.request.user.rh_org_has_subscription:
+            org_has_api_key = secret_manager.secret_exists(
+                self.request.user.organization.id, Suffixes.API_KEY
             )
             context["org_has_api_key"] = org_has_api_key
 
