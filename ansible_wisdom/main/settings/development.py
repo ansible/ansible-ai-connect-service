3061abcc9c7df68dafcd0fbe630bc2c8b8353841
Gonéri Le Bouder <goneri@lebouder.net>
2023-12-01 07:10:59 -0500

    dummy backend: accept an envvar with an empty string
    Right now, if I set `AUTHZ_DUMMY_ORGS_WITH_SUBSCRIPTION`, I will
    end-up with `11009103`. There is no way to get an empty string instead.
    
    This is what the PR address.

--- ansible_wisdom/main/settings/development.py
+++ ansible_wisdom/main/settings/development.py
@@ -66,18 +66,17 @@ AUTHZ_BACKEND_TYPE = os.getenv("AUTHZ_BACKEND_TYPE") or "dummy"
 # AUTHZ_DUMMY_USERS_WITH_SEAT=gleboude1@redhat.com
 # AUTHZ_DUMMY_RH_ORG_ADMINS=gleboude1@redhat.com
 # note: "*" means that all the users from org with a subscription.
-AUTHZ_DUMMY_USERS_WITH_SEAT = os.getenv("AUTHZ_DUMMY_USERS_WITH_SEAT") or "gleboude1@redhat.com"
-AUTHZ_DUMMY_RH_ORG_ADMINS = os.getenv("AUTHZ_DUMMY_RH_ORG_ADMINS") or "gleboude1@redhat.com"
+AUTHZ_DUMMY_USERS_WITH_SEAT = os.getenv("AUTHZ_DUMMY_USERS_WITH_SEAT", "")
+AUTHZ_DUMMY_RH_ORG_ADMINS = os.getenv("AUTHZ_DUMMY_RH_ORG_ADMINS", "")
 # You can get your account number on this page
 # https://www.redhat.com/wapps/ugc/protected/account.html
 # note: "*" means that all the orgs have a subscription.
-AUTHZ_DUMMY_ORGS_WITH_SUBSCRIPTION = os.getenv("AUTHZ_DUMMY_ORGS_WITH_SUBSCRIPTION") or "11009103"
+AUTHZ_DUMMY_ORGS_WITH_SUBSCRIPTION = os.getenv("AUTHZ_DUMMY_ORGS_WITH_SUBSCRIPTION", "")
 
-
-WCA_SECRET_BACKEND_TYPE = os.getenv("WCA_SECRET_BACKEND_TYPE") or "dummy"  # or aws_sm
+WCA_SECRET_BACKEND_TYPE = os.getenv("WCA_SECRET_BACKEND_TYPE", "dummy")  # or aws_sm
 # a list of key:value with a , separator. key is the orgid, value is the secret.
 # when a secret with the string "valid", it means the backend will accept it has
 # a valid string. e.g:
 # WCA_SECRET_DUMMY_SECRETS=1009103:valid,11009104:not-valid
-WCA_SECRET_DUMMY_SECRETS = os.getenv("WCA_SECRET_DUMMY_SECRETS") or ""
-WCA_CLIENT_BACKEND_TYPE = os.getenv("WCA_CLIENT_BACKEND_TYPE") or "dummy"  # or wcaclient
+WCA_SECRET_DUMMY_SECRETS = os.getenv("WCA_SECRET_DUMMY_SECRETS", "")
+WCA_CLIENT_BACKEND_TYPE = os.getenv("WCA_CLIENT_BACKEND_TYPE", "dummy")  # or wcaclient
