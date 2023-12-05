813421b7f391e415c431754c09bb62824921d6fd
Robin Y Bobbitt <rbobbitt@redhat.com>
2023-12-05 14:33:50 -0500

    handle non-normalized spacing in single task prompt

--- ansible_wisdom/ai/api/pipelines/completion_stages/pre_process.py
+++ ansible_wisdom/ai/api/pipelines/completion_stages/pre_process.py
@@ -52,6 +52,15 @@ def completion_pre_process(context: CompletionContext):
     context.payload.context, context.payload.prompt = fmtr.preprocess(
         payload_context, prompt, ansibleFileType, additionalContext
     )
+    if not multi_task:
+        # We are currently more forgiving on leading spacing of single task
+        # prompts than multi task prompts. In order to use the "original"
+        # single task prompt successfull in post-processing, we need to
+        # ensure its spacing aligns with the normalized context we got
+        # back from preprocess. We can calculate the proper spacing from the
+        # normalized prompt.
+        normalized_indent = len(context.payload.prompt) - len(context.payload.prompt.lstrip())
+        original_prompt = " " * normalized_indent + original_prompt.lstrip()
     context.payload.original_prompt = original_prompt
 
 
