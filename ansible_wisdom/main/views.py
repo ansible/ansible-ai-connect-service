1a378bb9308a7fc6fdd5e6f4649018506ca82380
Kirill Gaevskii <kgaevski@redhat.com>
2023-12-08 08:21:14 +0100

    Redirect on log out to appropriate social log out (#721)
    * Redirect on log out to appropriate social log out
    
    * Do not navigate user on logout if logged with Github
    
    * Update logout link and more tests

--- ansible_wisdom/main/views.py
+++ ansible_wisdom/main/views.py
@@ -11,6 +11,7 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.http import HttpResponseRedirect
 from main.base_views import ProtectedTemplateView
+from main.settings.base import SOCIAL_AUTH_OIDC_KEY
 from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
 from rest_framework.permissions import IsAuthenticated
 
@@ -22,6 +23,21 @@ class LoginView(auth_views.LoginView):
         if self.request.user.is_authenticated:
             return HttpResponseRedirect("/")
         return super().dispatch(request, *args, **kwargs)
+
+
+class LogoutView(auth_views.LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        self.next_page = self.get_next_page(request)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_next_page(self, request):
+        rht = (
+            'https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/logout'
+            f'?post_logout_redirect_uri={request.build_absolute_uri("/")}'
+            f'&client_id={SOCIAL_AUTH_OIDC_KEY}'
+        )
+
+        return rht if request.user.is_oidc_user() else None
 
 
 class ConsoleView(ProtectedTemplateView):
