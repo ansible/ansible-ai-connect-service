52896f3f742667df331d7054a3ecc65adc4c00ac
Michael Anstis <manstis@users.noreply.github.com>
2024-01-19 22:37:03 +0000

    Ensure organizations_organization is populated for existing Users. (#783)
    * Ensure organizations_organization is populated for existing Users.
    
    * Exclude organizations/migrations from SonarCloud test coverage checks.
    
    * Add unit test for organizations migrations.
    
    * Refactor tests to include additional scenario.
    
    ---------
    
    Co-authored-by: Michael Anstis <manstis@redhat.com>

---
+++ ansible_wisdom/organizations/migrations/0002_user_organization_fix_id.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [('organizations', '0001_initial'), ('users', '0009_user_organization')]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            insert into
+            organizations_organization
+            select
+            organization_id, false
+            from
+            users_user
+            where
+            organization_id is not null
+            on conflict (id) do nothing;
+            update
+            users_user
+            set
+            fk_organization_id = organization_id
+            where
+            organization_id is not null;
+            """
+        )
+    ]
---
+++ ansible_wisdom/organizations/migrations/tests/test_0002_user_organization_fix_id.py
@@ -0,0 +1,41 @@
+from django_test_migrations.contrib.unittest_case import MigratorTestCase
+
+
+class BaseOrganizationMigrationTest(MigratorTestCase):
+    def prepare(self):
+        user = self.old_state.apps.get_model('users', 'User')
+        user.objects.create(organization_id=123)
+
+    def assert_organization_relationship(self):
+        user_new = self.new_state.apps.get_model('users', 'User')
+        users = user_new.objects.all()
+        self.assertEqual(1, len(users))
+        self.assertTrue(hasattr(users[0], 'organization'))
+        self.assertIsNotNone(users[0].organization)
+        self.assertEqual(123, users[0].organization.id)
+
+        organization_new = self.new_state.apps.get_model('organizations', 'Organization')
+        organizations = organization_new.objects.all()
+        self.assertEqual(1, len(organizations))
+        self.assertTrue(hasattr(users[0], 'organization'))
+        self.assertEqual(organizations[0].id, users[0].organization.id)
+
+
+class TestOrganizationMigration(BaseOrganizationMigrationTest):
+    migrate_from = ('organizations', '0001_initial')
+    migrate_to = ('organizations', '0002_user_organization_fix_id')
+
+    def test_migration_0002_user_organization_fix_id(self):
+        user_old = self.old_state.apps.get_model('users', 'User')
+        self.assertFalse(hasattr(user_old, 'organization'))
+        self.assert_organization_relationship()
+
+
+class TestUserOrganizationMigration(BaseOrganizationMigrationTest):
+    migrate_from = ('users', '0009_user_organization')
+    migrate_to = ('organizations', '0002_user_organization_fix_id')
+
+    def test_migration_0002_user_organization_fix_id(self):
+        user_old = self.old_state.apps.get_model('users', 'User')
+        self.assertTrue(hasattr(user_old, 'organization'))
+        self.assert_organization_relationship()
