f4e3cf49fd6f3bff93a23efcb59d4c1124e911ba
James Wong <2421248+jameswnl@users.noreply.github.com>
2024-02-14 09:11:46 -0500

    dummy instead of mock for model mesh type

--- ansible_wisdom/main/settings/base.py
+++ ansible_wisdom/main/settings/base.py
@@ -29,7 +29,7 @@ ANSIBLE_AI_MODEL_MESH_INFERENCE_URL = (
     f"{ANSIBLE_AI_MODEL_MESH_HOST}:{ANSIBLE_AI_MODEL_MESH_INFERENCE_PORT}"
 )
 
-ANSIBLE_AI_MODEL_MESH_API_TYPE: Literal["grpc", "http", "mock", "wca"] = os.getenv(
+ANSIBLE_AI_MODEL_MESH_API_TYPE: Literal["grpc", "http", "dummy", "wca"] = os.getenv(
     "ANSIBLE_AI_MODEL_MESH_API_TYPE", "http"
 )
 ANSIBLE_AI_MODEL_MESH_API_HEALTHCHECK_PROTOCOL = os.getenv(
