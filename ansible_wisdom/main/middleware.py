812143856ca8fc831c246b40b0449887c1b425e6
Kirill Gaevskii <kgaevski@redhat.com>
2024-02-28 16:50:59 +0100

    vscode extension provides wrong message on some 204s

--- ansible_wisdom/main/middleware.py
+++ ansible_wisdom/main/middleware.py
@@ -108,6 +108,7 @@ class SegmentMiddleware:
                     # Clean up response.data for 204
                     if response.status_code == 204:
                         response.data = None
+                        response['Content-Length'] = 0
                     # For other error cases, remove 'model' in response data
                     elif response.status_code >= 400:
                         response_data.pop('model', None)
