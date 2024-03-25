189f161754ce0ee31d4ef88034d5b1b8bf553019
Roger Martinez <roger600@gmail.com>
2024-03-25 13:56:15 +0100

    Fixes

--- ansible_wisdom/organizations/tests/test_organizations.py
+++ ansible_wisdom/organizations/tests/test_organizations.py
@@ -1,5 +1,8 @@
-from django.test import TestCase
+from unittest.mock import patch
 
+from django.test import TestCase, override_settings
+
+import ansible_wisdom.ai.feature_flags as feature_flags
 from ansible_wisdom.organizations.models import Organization
 
 
@@ -11,3 +14,45 @@ class TestOrganization(TestCase):
     def test_org_with_telemetry_schema_2_opted_out(self):
         organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
         self.assertTrue(organization.telemetry_opt_out)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_override(self, LDClient):
+        LDClient.return_value.variation.return_value = True
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
+        self.assertTrue(organization.telemetry_opt_out)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_no_override(self, LDClient):
+        LDClient.return_value.variation.return_value = False
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
+        self.assertTrue(organization.telemetry_opt_out)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_override(self, LDClient):
+        LDClient.return_value.variation.return_value = True
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
+        self.assertFalse(organization.telemetry_opt_out)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_no_override(self, LDClient):
+        LDClient.return_value.variation.return_value = False
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
+        self.assertFalse(organization.telemetry_opt_out)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_unlimited_access_allowed_with_feature_flag_override(self, LDClient):
+        LDClient.return_value.variation.return_value = True
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
+        self.assertTrue(organization.is_subscription_check_should_be_bypassed)
+
+    @override_settings(LAUNCHDARKLY_SDK_KEY='dummy_key')
+    @patch.object(feature_flags, 'LDClient')
+    def test_org_with_no_unlimited_access_allowed_with_feature_flag_no_override(self, LDClient):
+        LDClient.return_value.variation.return_value = False
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
+        self.assertFalse(organization.is_subscription_check_should_be_bypassed)
