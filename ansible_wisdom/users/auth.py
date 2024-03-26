c277a1f7229fa5e3a0a317117931bc5268284543
Kirill Gaevskii <kgaevski@redhat.com>
2024-03-26 13:53:37 +0100

    AAP-21571: Lightspeed on-prem to check AAP license

--- ansible_wisdom/users/auth.py
+++ ansible_wisdom/users/auth.py
@@ -28,6 +28,8 @@ class AAPOAuth2(BaseOAuth2):
     SCOPE_SEPARATOR = ','
     EXTRA_DATA = [('id', 'id'), ('expires', 'expires')]
 
+    is_license_expired = False
+
     def get_user_details(self, response):
         """Return user details"""
         return {
@@ -42,6 +44,25 @@ class AAPOAuth2(BaseOAuth2):
         url = f'{settings.AAP_API_URL}/v2/me/'
         resp_data = self.get_json(url, headers={"Authorization": f"bearer {access_token}"})
         return resp_data.get('results')[0]
+
+    def user_configs(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        url = f'{settings.AAP_API_URL}/v2/config/'
+        resp_data = self.get_json(url, headers={"Authorization": f"bearer {access_token}"})
+        return resp_data
+
+    def check_license_expiration(self, access_token, *args, **kwargs):
+        data = self.user_configs(access_token, *args, **kwargs)
+        return data['license_info']['date_expired']
+
+    def auth_complete(self, *args, **kwargs):
+        user = super().auth_complete(*args, **kwargs)
+        user.is_onprem_license_expired = self.is_license_expired
+        return user
+
+    def do_auth(self, access_token, *args, **kwargs):
+        self.is_license_expired = self.check_license_expiration(access_token, *args, **kwargs)
+        return super().do_auth(access_token, *args, **kwargs)
 
     def extra_data(self, user, uid, response, details=None, *args, **kwargs):
         """Return access_token, token_type, and extra defined names to store in
