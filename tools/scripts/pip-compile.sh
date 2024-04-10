70e9cddf32f570bb62c5a8530ef11b469422e8f0
Robin Y Bobbitt <rbobbitt@redhat.com>
2024-04-10 13:52:43 -0400

    python 3.11

--- tools/scripts/pip-compile.sh
+++ tools/scripts/pip-compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -o errexit
 
-/usr/bin/python3 -m venv /var/www/venv
-/var/www/venv/bin/python3 -m pip --no-cache-dir install pip-tools
+dnf install -y python3.11
+
+/usr/bin/python3.11 -m venv /var/www/venv
+/var/www/venv/bin/python3.11 -m pip --no-cache-dir install pip-tools
 
 TARGET=$(uname -m)
 /var/www/venv/bin/pip-compile -o requirements-${TARGET}.txt requirements.in
