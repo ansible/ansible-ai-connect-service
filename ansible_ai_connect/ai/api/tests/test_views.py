e175d4530ae36d86e2693ee577480b065d97a594
Gonéri Le Bouder <goneri@lebouder.net>
2024-07-03 17:51:35 -0400

    a

--- ansible_ai_connect/ai/api/tests/test_views.py
+++ ansible_ai_connect/ai/api/tests/test_views.py
@@ -324,7 +324,7 @@ class TestCompletionWCAView(WisdomAppsBackendMocking, WisdomServiceAPITestCaseBa
             self.assertTrue(len(segment_events) > 0)
             for event in segment_events:
                 properties = event["properties"]
-                self.assertEqual(properties["modelName"], "")
+                self.assertIsNone(properties["modelName"])
                 if event["event"] == "completion":
                     self.assertEqual(properties["response"]["status_code"], 403)
                 elif event["event"] == "prediction":
