a691c5e2b0137805c85c27b4e086cdf3d0c29f67
Gonéri Le Bouder <goneri@lebouder.net>
2024-08-08 11:55:04 -0400

    unittest: add to missing __init__.py files (#1250)
    From: https://docs.python.org/3/library/unittest.html
    
    ```
    Changed in version 3.11: unittest dropped the namespace packages support in Python 3.11.
    It has been broken since Python 3.7. Start directory and subdirectories containing tests
    must be regular package that have __init__.py file.
    ```
    
    Also fix the `TestOrganization` test failures that are caused by the lazy import.

--- ansible_ai_connect/organizations/tests/test_organizations.py
+++ ansible_ai_connect/organizations/tests/test_organizations.py
@@ -16,57 +16,81 @@ from unittest.mock import patch
 
 from django.test import TestCase, override_settings
 
-import ansible_ai_connect.ai.feature_flags as feature_flags
 from ansible_ai_connect.organizations.models import Organization
+
+
+def get_feature_flags_that_say_False():
+    class DummyFeatureFlags:
+        def check_flag(self, flag=None, query_dict=None):
+            return False
+
+    return DummyFeatureFlags()
+
+
+def get_feature_flags_that_say_True():
+    class DummyFeatureFlags:
+        def check_flag(self, flag=None, query_dict=None):
+            return True
+
+    return DummyFeatureFlags()
 
 
 class TestOrganization(TestCase):
     def test_org_with_telemetry_schema_2_opted_in(self):
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=False)[0]
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
         self.assertFalse(organization.has_telemetry_opt_out)
 
     def test_org_with_telemetry_schema_2_opted_out(self):
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=True)[0]
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
         self.assertTrue(organization.has_telemetry_opt_out)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_override(self, LDClient):
-        LDClient.return_value.variation.return_value = True
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=True)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags", get_feature_flags_that_say_True
+    )
+    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
         self.assertTrue(organization.has_telemetry_opt_out)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_no_override(self, LDClient):
-        LDClient.return_value.variation.return_value = False
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=True)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags",
+        get_feature_flags_that_say_False,
+    )
+    def test_org_with_telemetry_schema_2_opted_in_with_feature_flag_no_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=True)[0]
         self.assertTrue(organization.has_telemetry_opt_out)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_override(self, LDClient):
-        LDClient.return_value.variation.return_value = True
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=False)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags", get_feature_flags_that_say_True
+    )
+    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
         self.assertFalse(organization.has_telemetry_opt_out)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_no_override(self, LDClient):
-        LDClient.return_value.variation.return_value = False
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=False)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags",
+        get_feature_flags_that_say_False,
+    )
+    def test_org_with_telemetry_schema_2_opted_out_with_feature_flag_no_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
         self.assertFalse(organization.has_telemetry_opt_out)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_unlimited_access_allowed_with_feature_flag_override(self, LDClient):
-        LDClient.return_value.variation.return_value = True
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=False)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags", get_feature_flags_that_say_True
+    )
+    def test_org_with_unlimited_access_allowed_with_feature_flag_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
         self.assertTrue(organization.is_subscription_check_should_be_bypassed)
 
     @override_settings(LAUNCHDARKLY_SDK_KEY="dummy_key")
-    @patch.object(feature_flags, "LDClient")
-    def test_org_with_no_unlimited_access_allowed_with_feature_flag_no_override(self, LDClient):
-        LDClient.return_value.variation.return_value = False
-        organization = Organization.objects.get_or_create(id=123, has_telemetry_opt_out=False)[0]
+    @patch(
+        "ansible_ai_connect.organizations.models.get_feature_flags",
+        get_feature_flags_that_say_False,
+    )
+    def test_org_with_no_unlimited_access_allowed_with_feature_flag_no_override(self):
+        organization = Organization.objects.get_or_create(id=123, telemetry_opt_out=False)[0]
         self.assertFalse(organization.is_subscription_check_should_be_bypassed)
