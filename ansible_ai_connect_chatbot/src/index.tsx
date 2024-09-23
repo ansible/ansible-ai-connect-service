8d8e4760579f93fe7d564498c829409b4c6b3340
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-09-23 10:55:21 -0400

    Update index.tsx (#1321)
    Reorder imports to prevent style overrides

--- ansible_ai_connect_chatbot/src/index.tsx
+++ ansible_ai_connect_chatbot/src/index.tsx
@@ -1,22 +1,19 @@
-import React from "react";
-import "@patternfly/react-core/dist/styles/base.css";
-// import "@patternfly/patternfly/patternfly-addons.css";
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import '@patternfly/react-core/dist/styles/base.css';
+// import '@patternfly/patternfly/patternfly-addons.css';
 
 // Add your extension CSS below
-import "@patternfly/virtual-assistant/dist/css/main.css";
-
-import ReactDOM from "react-dom/client";
-// import './index.css';
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+import '@patternfly/virtual-assistant/dist/css/main.css';
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
