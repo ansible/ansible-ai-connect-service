882d54e5204842453ba8179b645d860711541c7b
Gonéri Le Bouder <goneri@lebouder.net>
2024-10-10 14:46:21 -0400

    AAP-30618: refactoring: migrate PlaybookExplanation to the new Schema1 object

--- ansible_ai_connect/ai/api/telemetry/schema1.py
+++ ansible_ai_connect/ai/api/telemetry/schema1.py
@@ -15,10 +15,12 @@
 
 import logging
 import platform
+import time
 
 from attr import Factory, asdict, field
 from attrs import define, validators
 from django.utils import timezone
+from yaml.error import MarkedYAMLError
 
 from ansible_ai_connect.healthcheck.version_info import VersionInfo
 from ansible_ai_connect.users.models import User
@@ -31,6 +33,15 @@ version_info = VersionInfo()
 class RequestPayload:
     method: str = field(validator=validators.instance_of(str), converter=str, default="")
     path: str = field(validator=validators.instance_of(str), converter=str, default="")
+
+
+@define
+class ResponsePayload:
+    exception: str = field(validator=validators.instance_of(str), converter=str, default="")
+    error_type: str = field(validator=validators.instance_of(str), converter=str, default="")
+    message: str = field(validator=validators.instance_of(str), converter=str, default="")
+    status_code: int = field(validator=validators.instance_of(int), converter=int, default=0)
+    status_text: str = field(validator=validators.instance_of(str), converter=str, default="")
 
 
 @define
@@ -73,22 +84,42 @@ class Schema1Event:
     problem: str = field(validator=validators.instance_of(str), converter=str, default="")
     exception: bool = False
     request: RequestPayload = RequestPayload()
+    response: ResponsePayload = ResponsePayload()
     _user: User | None = None
+    _created_at: int = time.time()
+    plans: list[PlanEntry] = Factory(list)
+
+    def set_exception(self, exception):
+        if not exception:
+            return
+        self.exception = True
+        self.response.exception = str(exception)
+        self.problem = (
+            exception.problem
+            if isinstance(exception, MarkedYAMLError)
+            else str(exception) if str(exception) else exception.__class__.__name__
+        )
 
     def set_user(self, user):
         self._user = user
         self.rh_user_has_seat = user.rh_user_has_seat
         self.rh_user_org_id = user.org_id
         self.groups = list(user.groups.values_list("name", flat=True))
+        self.plans = [PlanEntry.init(up) for up in user.userplan_set.all()]
 
     def set_request(self, request):
         self.set_user(request.user)
         self.request = RequestPayload(path=request.path, method=request.method)
 
+    def set_duration(self):
+        self.duration = round((time.time() - self._created_at) * 1000, 2)
+
     def as_dict(self):
-        # NOTE: The allowed fields should be moved in the event class itslef
+        if hasattr(self, "duration") and not self.duration:
+            self.set_duration()
+
         def my_filter(a, v):
-            return a.name not in ["_user", "event_name"]
+            return a.name not in ["_user", "event_name", "_created_at"]
 
         return asdict(self, filter=my_filter, recurse=True)
 
@@ -96,8 +127,11 @@ class Schema1Event:
 @define
 class OneClickTrialStartedEvent(Schema1Event):
     event_name: str = "oneClickTrialStarted"
-    plans: list[PlanEntry] = Factory(list)
 
-    def set_user(self, user):
-        super().set_user(user)
-        self.plans = [PlanEntry.init(up) for up in user.userplan_set.all()]
+
+@define
+class ExplainPlaybookEvent(Schema1Event):
+    event_name: str = "explainPlaybook"
+    duration: float | None = None
+    playbook_length: int = field(validator=validators.instance_of(int), default=0)
+    explanationId: str = field(validator=validators.instance_of(str), converter=str, default="")
