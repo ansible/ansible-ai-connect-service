2f458dd7907e544c81c1ceb4b40eb9f9479f9932
Aykut M. Bulgu <abulgu@redhat.com>
2024-10-11 18:17:48 +0300

    AAP-30986: Support deletion of WCA API_KEY from Admin Portal (Frontend) (#1333)

---
+++ ansible_ai_connect_admin_portal/config/webpack.mock.api.keys.modelids.ts
@@ -0,0 +1,132 @@
+import {
+  delay,
+  DELAY_MS,
+  ERROR_DESCRIPTION,
+  ORG_ID,
+} from "./webpack.mock.globals";
+
+const webpackMockServer = require("webpack-mock-server");
+
+export default webpackMockServer.add((app, helper) => {
+  const KEY_FIELD = "key";
+  const MODEL_ID_FIELD = "model_id";
+  const LAST_UPDATED_FIELD = "last_update";
+
+  const keys = new Map<string, { key: string; last_update: Date }>();
+  const modelIds = new Map<string, { model_id: string; last_update: Date }>();
+
+  //API KEY RELATED ENDPOINTS
+  app.get("/api/v0/wca/apikey/", async (_req, res) => {
+    await delay(DELAY_MS);
+    if (keys.has(ORG_ID)) {
+      // Key response only contains the Last Updated field
+      const data = keys.get(ORG_ID);
+      res.json({ last_update: data[LAST_UPDATED_FIELD] });
+    } else {
+      res.json({});
+    }
+  });
+
+  app.get("/api/v0/wca/apikey/test/", async (_req, res) => {
+    await delay(DELAY_MS);
+    if (!keys.has(ORG_ID)) {
+      res.sendStatus(404);
+      return;
+    } else {
+      const data = keys.get(ORG_ID);
+      const key = data[KEY_FIELD];
+      if (key === "error-test") {
+        // Emulate a server-side error
+        res.status(500).json({ detail: ERROR_DESCRIPTION });
+        return;
+      }
+      if (key === "invalid-test") {
+        // Emulate an invalid API Key
+        res.sendStatus(400);
+        return;
+      }
+    }
+    res.sendStatus(200);
+  });
+
+  app.post("/api/v0/wca/apikey/", async (_req, res) => {
+    await delay(DELAY_MS);
+    const key = _req.body["key"];
+    if (key === "error") {
+      // Emulate a server-side error
+      res.status(500).json({ detail: ERROR_DESCRIPTION });
+      return;
+    }
+    if (key === "invalid") {
+      // Emulate an invalid API Key
+      res.sendStatus(400);
+      return;
+    }
+    const entry = { key: key, last_update: new Date() };
+    keys.set(ORG_ID, entry);
+    res.sendStatus(200);
+  });
+
+  app.delete("/api/v0/wca/apikey/", async (_req, res) => {
+    await delay(DELAY_MS);
+    if (keys.has(ORG_ID)) {
+      keys.delete(ORG_ID);
+      if (modelIds.has(ORG_ID)) {
+        modelIds.delete(ORG_ID);
+      }
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(400);
+    }
+  });
+
+  //MODEL ID RELATED ENDPOINTS
+  app.get("/api/v0/wca/modelid/", async (_req, res) => {
+    await delay(DELAY_MS);
+    if (modelIds.has(ORG_ID)) {
+      res.json(modelIds.get(ORG_ID));
+    } else {
+      res.json({});
+    }
+  });
+
+  app.get("/api/v0/wca/modelid/test/", async (_req, res) => {
+    await delay(DELAY_MS);
+    if (!modelIds.has(ORG_ID)) {
+      res.sendStatus(404);
+      return;
+    } else {
+      const data = modelIds.get(ORG_ID);
+      const modelId = data[MODEL_ID_FIELD];
+      if (modelId === "error-test") {
+        // Emulate a server-side error
+        res.status(500).json({ detail: ERROR_DESCRIPTION });
+        return;
+      }
+      if (modelId === "invalid-test") {
+        // Emulate an invalid Model Id
+        res.sendStatus(400);
+        return;
+      }
+    }
+    res.sendStatus(200);
+  });
+
+  app.post("/api/v0/wca/modelid/", async (_req, res) => {
+    await delay(DELAY_MS);
+    const modelId = _req.body[MODEL_ID_FIELD];
+    if (modelId === "error") {
+      // Emulate a server-side error
+      res.status(500).json({ detail: ERROR_DESCRIPTION });
+      return;
+    }
+    if (modelId === "invalid") {
+      // Emulate an invalid Model Id
+      res.sendStatus(400);
+      return;
+    }
+    const entry = { model_id: modelId, last_update: new Date() };
+    modelIds.set(ORG_ID, entry);
+    res.sendStatus(200);
+  });
+});
---
+++ ansible_ai_connect_admin_portal/src/ModelSettingsKeyDeletionModal.tsx
@@ -0,0 +1,87 @@
+import { useTranslation } from "react-i18next";
+import { Button, Modal, ModalVariant, Text } from "@patternfly/react-core";
+import { useState } from "react";
+import { DELAY } from "./api/globals";
+import { APIException } from "./api/types";
+import { deleteWcaKey } from "./api/api";
+import { AxiosError } from "axios";
+import { HasError, NO_ERROR } from "./ErrorModal";
+
+interface ModelSettingsKeyDeletionModalProps {
+  readonly isModalOpen: boolean;
+  readonly setIsModalOpen: (isOpen: boolean) => void;
+  readonly handleModalToggle: () => void;
+  readonly reloadParent: () => void;
+}
+
+export const ModelSettingsKeyDeletionModal = (
+  props: ModelSettingsKeyDeletionModalProps,
+) => {
+  const { t } = useTranslation();
+  const { isModalOpen, setIsModalOpen, handleModalToggle, reloadParent } =
+    props;
+  const [isDeleting, setDeleting] = useState<boolean>(false);
+  const [keyError, setKeyError] = useState<HasError>(NO_ERROR);
+
+  const deleteKey = () => {
+    const timeoutId = setTimeout(() => setDeleting(true), DELAY);
+    deleteWcaKey()
+      .then((_) => {
+        reloadParent();
+        handleCancel();
+      })
+      .catch((error: AxiosError<APIException, any>) => {
+        setKeyError({
+          inError: true,
+          message: error.message,
+          detail: error.response?.data?.detail ?? "",
+        });
+      })
+      .finally(() => {
+        setDeleting(false);
+        clearTimeout(timeoutId);
+      });
+  };
+
+  const handleCancel = () => {
+    setKeyError({
+      inError: false,
+    });
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal
+      variant={ModalVariant.small}
+      title="Delete Confirmation"
+      isOpen={isModalOpen}
+      onClose={handleModalToggle}
+      data-testid={"model-settings-key-deletion-modal"}
+      actions={[
+        <Button
+          key="delete"
+          variant="danger"
+          onClick={deleteKey}
+          isDisabled={isDeleting}
+          data-testid={"model-settings-key-deletion-modal__delete-button"}
+        >
+          {t("Delete")}
+        </Button>,
+        <Button key="cancel" variant="link" onClick={handleCancel}>
+          {t("Cancel")}
+        </Button>,
+      ]}
+    >
+      {t("APIKeyDeletionConfirmation")}
+      {keyError.inError && (
+        <Text
+          component="p"
+          style={{ color: "red" }}
+          data-testid={"model-settings-key-deletion-modal__error-text"}
+        >
+          {t("KeyDeletionError")}: {keyError.message}
+        </Text>
+      )}
+    </Modal>
+  );
+};
