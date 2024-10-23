a70d1083b55bc383acd4221c2c48685a77959c3f
Tami Takamiya <ttakamiy@redhat.com>
2024-10-23 10:51:26 -0400

    Duplicated chatbot message box issue

--- ansible_ai_connect/main/settings/base.py
+++ ansible_ai_connect/main/settings/base.py
@@ -539,6 +539,7 @@ WCA_ENABLE_ARI_POSTPROCESS = os.getenv("WCA_ENABLE_ARI_POSTPROCESS", "False").lo
 CSP_DEFAULT_SRC = ("'self'", "data:")
 CSP_INCLUDE_NONCE_IN = ["script-src-elem"]
 CSP_CONNECT_SRC = "'self'"
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 
 # Region for where the service is deployed. Used by the Health Check endpoint.
 DEPLOYED_REGION = os.getenv("DEPLOYED_REGION") or "unknown"
