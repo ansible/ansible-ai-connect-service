f24089a6b9cbd9bf158e4f41bf3af81a1641e224
Tami Takamiya <ttakamiy@redhat.com>
2024-11-19 08:18:06 -0500

    Fix issues on Verify accuracy footnote

--- ansible_ai_connect_chatbot/src/AnsibleChatbot/AnsibleChatbot.tsx
+++ ansible_ai_connect_chatbot/src/AnsibleChatbot/AnsibleChatbot.tsx
@@ -52,21 +52,24 @@ import {
   ChatbotToggle,
   FileDropZone,
 } from "@patternfly/virtual-assistant";
+import {
+  FOOTNOTE_DESCRIPTION,
+  FOOTNOTE_LABEL,
+  FOOTNOTE_TITLE,
+} from "../Constants";
 
 const footnoteProps = {
-  label: "Lightspeed uses AI. Check for mistakes.",
+  label: FOOTNOTE_LABEL,
   popover: {
-    title: "Verify accuracy",
-    description: `While Lightspeed strives for accuracy, there's always a possibility of errors. It's a good practice to verify critical information from reliable sources, especially if it's crucial for decision-making or actions.`,
+    title: FOOTNOTE_TITLE,
+    description: FOOTNOTE_DESCRIPTION,
     bannerImage: {
-      src: "https://cdn.dribbble.com/userupload/10651749/file/original-8a07b8e39d9e8bf002358c66fce1223e.gif",
-      alt: "Example image for footnote popover",
+      src: lightspeedLogo,
+      alt: "Lightspeed logo",
     },
     cta: {
       label: "Got it",
-      onClick: () => {
-        alert("Do something!");
-      },
+      onClick: () => {},
     },
     link: {
       label: "Learn more",
