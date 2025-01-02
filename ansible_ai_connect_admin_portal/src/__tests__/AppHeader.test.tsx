184e53a6f3812d338317746e8cbdf6d6eab6b899
Michael Anstis <manstis@redhat.com>
2025-01-02 10:58:11 +0000

    AAP-36512: FIx Admin portal tests fallout due to PF5 and latest Ansible-UI-framework

--- ansible_ai_connect_admin_portal/src/__tests__/AppHeader.test.tsx
+++ ansible_ai_connect_admin_portal/src/__tests__/AppHeader.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { AppHeader } from "../AppHeader";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { act } from "react";
 
 describe("AppHeader", () => {
   // Store the original 'location' object so that it can be restored for other tests.
@@ -33,5 +35,16 @@ describe("AppHeader", () => {
 
     // Check "Logout" option is not present
     expect(screen.queryByText("Logout")).toBeNull();
+
+    // Emulate click on menu button
+    await act(() => userEvent.click(accountMenu));
+
+    // "Logout" menu option should now be present
+    const logoutMenuButton = await screen.findByText("Logout");
+    expect(logoutMenuButton).toBeInTheDocument();
+
+    // Emulate clicking on the logout button
+    await act(() => userEvent.click(logoutMenuButton));
+    expect(window.location.assign).toBeCalledWith("/logout");
   });
 });
