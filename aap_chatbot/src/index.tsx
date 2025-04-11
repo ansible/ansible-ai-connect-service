70e3f4fabea70afef0c8734ae269f123042b376e
Sumit Jaiswal <sjaiswal@redhat.com>
2025-04-11 18:36:15 +0530

    export for making lightspeed logo work (#1618)

--- aap_chatbot/src/index.tsx
+++ aap_chatbot/src/index.tsx
@@ -4,3 +4,7 @@ import "@patternfly/chatbot/dist/css/main.css";
 
 export { App } from "./App";
 export { ANSIBLE_LIGHTSPEED_PRODUCT_NAME } from "./Constants";
+import LIGHTSPEED_LOGO from "./public/lightspeed.svg";
+export { LIGHTSPEED_LOGO };
+import LIGHTSPEED_LOGO_DARK from "./public/lightspeed_dark.svg";
+export { LIGHTSPEED_LOGO_DARK };
