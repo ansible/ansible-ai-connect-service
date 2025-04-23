f37e02f0e78d3527e2502603ca144750be9bb736
Sumit Jaiswal <sjaiswal@redhat.com>
2025-04-23 17:56:23 +0530

    to fix the testathon new line breaks issue (#1628)

--- aap_chatbot/src/useChatbot/useChatbot.ts
+++ aap_chatbot/src/useChatbot/useChatbot.ts
@@ -84,7 +84,7 @@ export const feedbackMessage = (
     f.sentiment === Sentiment.THUMBS_UP
       ? "Thank you for your feedback!"
       : "Thank you for your feedback. If you have more to share, please click the button below (_requires GitHub login_). " +
-        "\\n\\n Do not include any personal information or other sensitive information in your feedback. " +
+        "\n\n Do not include any personal information or other sensitive information in your feedback. " +
         "Feedback may be used to improve Red Hat's products or services.",
   name: botName,
   avatar: logo,
