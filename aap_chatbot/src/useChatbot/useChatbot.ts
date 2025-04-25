3531c014f01543e7c7f695076528c0e48bee2341
Tami Takamiya <ttakamiy@redhat.com>
2025-04-25 16:36:53 -0400

    Adjust avatar icon size

--- aap_chatbot/src/useChatbot/useChatbot.ts
+++ aap_chatbot/src/useChatbot/useChatbot.ts
@@ -285,7 +285,6 @@ export const useChatbot = () => {
       content: typeof response === "object" ? response.response : response,
       name: botName,
       avatar: logo,
-      hasRoundAvatar: false,
       timestamp: getTimestamp(),
       referenced_documents: [],
     };
