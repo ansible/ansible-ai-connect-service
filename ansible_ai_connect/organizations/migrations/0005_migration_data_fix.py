b5393c86d0e26b29bd742cc03bc97bf492c05891
AlanCoding <arominge@redhat.com>
2025-09-25 10:10:03 -0400

    do not know why Im doing this becaause nothing works

--- ansible_ai_connect/organizations/migrations/0005_migration_data_fix.py
+++ ansible_ai_connect/organizations/migrations/0005_migration_data_fix.py
@@ -1,6 +1,9 @@
 # Generated manually to create new Organization model and clear dab_rbac data
 
+import logging
 from django.db import migrations
+
+logger = logging.getLogger(__name__)
 
 
 def clear_dab_rbac_data(apps, schema_editor):
@@ -14,6 +17,13 @@ def clear_dab_rbac_data(apps, schema_editor):
 
     # Delete all data from dab_rbac models in reverse dependency order
     for model in reversed(dab_rbac_config.get_models()):
+        count = model.objects.using(db_alias).count()
+        if count > 0:
+            logger.warning(
+                "Migration 0005_migration_data_fix: Deleting %d entries from model %s",
+                count,
+                model.__name__
+            )
         model.objects.using(db_alias).all().delete()
 
 
